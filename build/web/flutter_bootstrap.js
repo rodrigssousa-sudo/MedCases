a75efe47b887d70b080cfe92f96bc407f0607ba8
Bruno Rodrigues <rodrigssousa@gmail.com>
2026-05-15 14:06:42 +0000

    build: web release atualizado — d77082e (Gemini fallback + Goodman & Gilman)

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "426699162"
+    serviceWorkerVersion: "2052087480"
   }
 });
