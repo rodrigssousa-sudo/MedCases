def56023ead59da7fa9d1ce49e6ad2fa0843ffca
rodrigssousa-sudo <genspark_dev@genspark.ai>
2026-06-24 17:05:31 +0000

    debug(render): trace plantao pipeline rebuilds build 231

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1323796245"
+    serviceWorkerVersion: "4069405687"
   }
 });
